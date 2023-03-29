015402de23efa3f7bc758b12fa45643ee9db4440
Edgar Esquivel <edgarmel95@gmail.com>
2023-03-29 13:53:28 -0600

    creacion de rodapie y footer

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,16 +1,23 @@
 window.onload= ()=>{
-    const slider = document.querySelector('.slider');
-    const slider__show = document.querySelector('.slider__show');
+    const $slider = document.querySelector('.slider');
+    const $slider__show = document.querySelector('.slider__show');
   
-    const slider_widht= slider__show.scrollWidth;
-    const container_width = slider.clientWidth;
+    let slider_widht= $slider__show.scrollWidth;
+    let container_width = $slider.clientWidth;
+
+    window.addEventListener("resize",totalWidth)
+
+    function totalWidth(){
+        slider_widht = $slider__show.scrollWidth;
+        container_width = $slider.clientWidth;
+    }
     
     function startScroll() {
         scrollInterval = setInterval(() => {    
-            if (slider__show.scrollLeft + slider__show.offsetWidth < slider_widht) {
-                slider__show.scrollLeft += container_width;
+            if ($slider__show.scrollLeft + $slider__show.offsetWidth < slider_widht) {
+                $slider__show.scrollLeft += container_width;
             }else{
-                slider__show.scrollLeft = 0;
+                $slider__show.scrollLeft = 0;
             }
         }, 
         3000);
@@ -22,7 +29,7 @@ window.onload= ()=>{
   
     startScroll();
   
-    slider.addEventListener('mouseenter', stopScroll);
-    slider.addEventListener('mouseleave', startScroll);
+    $slider.addEventListener('mouseenter', stopScroll);
+    $slider.addEventListener('mouseleave', startScroll);
   
   }
